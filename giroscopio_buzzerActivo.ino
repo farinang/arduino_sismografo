5237e3e29509a8a99d2de2c4ff3d6e1ba8df9f74
farinang <69328212+farinang@users.noreply.github.com>
2022-01-24 23:26:38 -0500

    Add files via upload

---
+++ giroscopio_buzzerActivo.ino
@@ -0,0 +1,92 @@
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+char tmp_str[7]; // temporary variable used in convert function
+
+int16_t gyro_x_r, gyro_y_r, gyro_z_r;
+bool bandera = false;
+int16_t desplazamiento_x, desplazamiento_y, desplazamiento_z;
+bool condicion1, condicion2, condicion3;
+int16_t tolerancia = 500;
+int led=7;
+int buzzer=8;
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+
+void setup() {
+  pinMode(buzzer,OUTPUT);
+  pinMode(led,OUTPUT);
+  digitalWrite(led,LOW);
+  digitalWrite(buzzer,LOW);
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.println(" Colocar.el.sensor.en.una.superficie.plana,Presionar.la.tecla.'e'.y.ENTER");
+}
+void loop() {
+  if(Serial.available() == 1){
+    Serial.print(Serial.available());
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+    Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+    Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+    
+    // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+    accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+    accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+    accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+    temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+    gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+    gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+    gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+    gyro_x_r = gyro_x;
+    gyro_y_r = gyro_y;
+    gyro_z_r = gyro_z;
+    bandera = true;
+  }
+  if(bandera){
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+    Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+    Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+    
+    // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+    accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+    accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+    accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+    temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+    gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+    gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+    gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+    
+    desplazamiento_x = abs(gyro_x - gyro_x_r);
+    desplazamiento_y = abs(gyro_y - gyro_y_r);
+    desplazamiento_z = abs(gyro_z - gyro_z_r);
+    condicion1 = desplazamiento_x > tolerancia;
+    condicion2 = desplazamiento_y > tolerancia;
+    condicion3 = desplazamiento_z > tolerancia;
+  
+    Serial.print(convert_int16_to_str(gyro_z));Serial.print(","); 
+    Serial.print(convert_int16_to_str(gyro_y)); Serial.print(","); 
+    Serial.print(convert_int16_to_str(gyro_x));
+    Serial.println();
+    
+    if(condicion1 or condicion2 or condicion3){
+      digitalWrite(led,HIGH);
+      digitalWrite(buzzer,HIGH);
+      delay(3000);
+      digitalWrite(led,LOW);
+      digitalWrite(buzzer,LOW);
+    }
+    delay(20);
+  }
+}
